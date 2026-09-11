7e82276653edf12f7f80cbc14b52abbb7beec59d
Ahsan Habib <habibahsan350@gmail.com>
2026-09-11 17:14:26 +0600

    update wallpaper selector

--- config_dotfiles/config/custom_scripts/chooseBackgroundImage.sh
+++ config_dotfiles/config/custom_scripts/chooseBackgroundImage.sh
@@ -1,25 +1,90 @@
 #!/usr/bin/env bash
 
-get_wall_name() {
-  local backgrounds_directory="$HOME/Pictures/backgrounds"
-  local images=()
+backgrounds_directory="$HOME/Pictures/backgrounds"
+theme="$HOME/.config/rofi/themes/wallpicker.rasi"
+cache_file="$HOME/.cache/wall.txt"
 
-  while IFS= read -r file; do
-      name="${file##*/}"
-      images+=("$name")
-  done < <(find "$backgrounds_directory" -maxdepth 1 -type f \( \
-      -iname "*.jpg" -o \
-      -iname "*.jpeg" -o \
-      -iname "*.png" -o \
-      -iname "*.gif" -o \
-      -iname "*.bmp" -o \
-      -iname "*.webp" \
-  \))
-  selected_wall="$(printf '%s\n' "${images[@]}" | sort | rofi -dmenu -p "Walls")"
-  [[ -z "$selected_wall" ]] && exit 1
-  echo "$selected_wall" > "$HOME/.cache/wall.txt"
-  pkill swaybg
-  swaybg -i "$backgrounds_directory/$selected_wall" -m fill >/dev/null 2>&1 &
+
+show_wallpapers() {
+# - keep the current selection
+# - clear the filter after selecting
+printf '\0keep-selection\x1ftrue\n'
+
+local file
+local name
+
+while IFS= read -r -d '' file; do
+  name="${file##*/}"
+
+  printf '%s\0icon\x1f%s\n' \
+    "$name" \
+    "$file"
+done < <(
+  find "$backgrounds_directory" \
+    -maxdepth 1 \
+    -type f \
+    \( \
+    -iname "*.jpg" -o \
+    -iname "*.jpeg" -o \
+    -iname "*.png" -o \
+    -iname "*.gif" -o \
+    -iname "*.bmp" -o \
+    -iname "*.webp" \
+    \) \
+    -print0 |
+    sort -z
+  )
 }
 
-get_wall_name
+set_wallpaper() {
+  local selected_wall="$1"
+  [[ -z "$selected_wall" ]] && return
+  echo "$selected_wall" > "$cache_file"
+  pkill swaybg
+  swaybg \
+    -i "$backgrounds_directory/$selected_wall" \
+    -m fill \
+    >/dev/null 2>&1 &
+}
+
+if [[ -n "$ROFI_RETV" ]]; then
+  case "$ROFI_RETV" in
+    # Initial call
+    0)
+    show_wallpapers
+    ;;
+    1)
+    set_wallpaper "$1"
+    show_wallpapers
+    ;;
+  esac
+  exit 0
+fi
+
+if ! find "$backgrounds_directory" \
+  -maxdepth 1 \
+  -type f \
+  \( \
+  -iname "*.jpg" -o \
+  -iname "*.jpeg" -o \
+  -iname "*.png" -o \
+  -iname "*.gif" -o \
+  -iname "*.bmp" -o \
+  -iname "*.webp" \
+  \) \
+  -print -quit |
+  grep -q .
+then
+  notify-send \
+    "Wallpaper Picker" \
+    "No wallpapers found."
+
+  exit 1
+fi
+
+
+exec rofi \
+  -show walls \
+  -modi "walls:$0" \
+  -show-icons \
+  -theme "$theme"
